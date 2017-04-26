c2f85fc7f8b9ff8e520228de0e620a3bc6f04ca5
drewboardman <drew.e.boardman@gmail.com>
2017-04-26 22:43:19 +0000

    almost done with db stuffs ' '

--- ex17/ex17.c
+++ ex17/ex17.c
@@ -27,6 +27,13 @@ struct Connection {
 };
 
 void die(const char *message) {
+  if (errno) {
+    perror(message);
+  } else {
+    printf("Error: %s\n", message);
+  };
+
+  exit(1);
 };
 
 void Address_print(struct Address *addr) {
@@ -40,4 +47,129 @@ void Database_load(struct Connection *conn) {
   };
 };
 
-int main(int argc, char *argv[]){};
+struct Connection *Database_open(const char *filename, char mode) {
+  struct Connection *conn = malloc(sizeof(struct Connection));
+
+  if (!conn) {
+    die("Memory Error");
+  };
+
+  // set the database attribute of connection
+  conn->db = malloc(sizeof(struct Database));
+  if (!conn->db) {
+    die("Memory error");
+  };
+
+  if (mode == 'c') {
+    conn->file = fopen(filename, "w");
+  } else {
+    conn->file = fopen(filename, "r+");
+
+    if (conn->file) {
+      Database_load(conn);
+    };
+  };
+
+  if (!conn->file) {
+    die("Failed to open the file");
+  };
+
+  return(conn);
+};
+
+void Database_close(struct Connection *conn) {
+  if (conn) {
+    if (conn->file) {
+      fclose(conn->file);
+    };
+    if (conn->db) {
+      free(conn->db);
+    };
+    free(conn);
+  };
+};
+
+void Database_write(struct Connection *conn) {
+  rewind(conn->file);
+
+  int rc = fwrite(conn->db, sizeof(struct Database), 1, conn->file);
+
+  if (rc != 1) {
+    die("Failed to write database");
+  };
+
+  rc = fflush(conn->file);
+  if (rc == -1) {
+    die("Cannot flush db");
+  };
+};
+
+void Database_create(struct Connection *conn) {
+  int i = 0;
+
+  for (i=0; i < MAX_ROWS; i++) {
+    struct Address addr = {.id = i, .set = 0};
+    conn->db->rows[i] = addr;
+  };
+};
+
+void Database_set(struct Connection *conn, int id, const char *name, const char *email) {
+  struct Address *addr = &conn->db->rows[id];
+
+  if (addr->set) {
+    die("Already set, delete it first");
+  };
+
+  addr->set = 1;
+  // bug here; fix in "how to break it"
+  char *res = strncpy(addr->name, name, MAX_DATA);
+
+  if (!name) {
+    die("Name copy failed");
+  };
+
+  res = strncpy(addr->email, email, MAX_DATA);
+  if (!email) {
+    die("Email copy failed");
+  };
+};
+
+void Database_get(struct Connection *conn, int id) {
+  struct Address *addr = &conn->db->rows[id];
+
+  if (addr->set) {
+    Address_print(addr);
+  } else {
+    die("ID is not set");
+  };
+};
+
+void Database_delete(struct Connection *conn, int id) {
+  struct Address addr = {.id = id, .set = 0};
+  conn->db->rows[id] = addr;
+};
+
+void Database_list(struct Connection *conn) {
+  struct Database *db = conn->db;
+  int i = 0;
+
+  for (i=0; i < MAX_ROWS; i++) {
+    struct Address *current_address = &db->rows[i];
+
+    if(current_address->set) {
+      Address_print(current_address);
+    };
+  };
+};
+
+int main(int argc, char *argv[]){
+  if (argc < 3) {
+    die("USAGE: ex17 <dbfile> <action> [action params]");
+  };
+
+  char *filename = argv[1];
+  char action = argv[2][0];
+
+  struct Connection *conn = Database_open(filename, action);
+  int id = 0;
+};
--- ex17/notes.md
+++ ex17/notes.md
@@ -9,3 +9,10 @@ Heap vs Stack
   3. Get `die` to work
   3. Get `open` to work in little bits
 
+### Some notes about the program
+  - `errno` is the value given when a program returns a error code. The `man 3 errno` page says this is usually -1 or NULL
+
+  - `rewind` sets the file position indicator to the beginning of the file.
+
+### Questions
+  1. In `Database_open`, we start with creating a pointer to `*conn` using `malloc`. However, we always reference it without the `*` - like `conn->file`. Is this using the pointer or are we passing the actual `conn` piece of memory?
